0e00c0afd7009fd0c917df650f097e7492f01935
Habib Santoso <habib.santoso.ft18@mail.umy.ac.id>
2022-12-02 17:14:25 +0700

    menambahkan code di main untuk menampilkan menu

--- StackUsingLinkedList/Program.cs
+++ StackUsingLinkedList/Program.cs
@@ -76,6 +76,19 @@ namespace StackUsingLinkedList
     {
         static void Main(string[] args)
         {
+            Stack s = new Stack();
+            while (true)
+            {
+                Console.WriteLine();
+                Console.WriteLine("\n***Stack Menu***\n");
+                Console.WriteLine("1. Push");
+                Console.WriteLine("2. Pop");
+                Console.WriteLine("3. Display");
+                Console.WriteLine("4 Exit");
+                Console.Write("\nEnter your choice: ");
+                String sInput = Console.ReadLine();
+                char ch = Convert.ToChar(sInput == "" ? "0" : sInput);
+            }
         }
     }
 }
